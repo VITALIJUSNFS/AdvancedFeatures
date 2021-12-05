da42cab02a32d7bac0ec528c4b3ea761e0bd52c2
Vitalijus <vitalijus.kurtinas@gmail.com>
2021-12-05 11:45:42 +0200

    VERSION 2021 12 05 / 1 COMMIT1 Added logger

--- src/main/java/testingRecap/Calculator.java
+++ src/main/java/testingRecap/Calculator.java
@@ -1,5 +1,21 @@
 package testingRecap;
 
+import org.apache.log4j.Logger;
+
 public class Calculator {
 
+    private static final Logger LOGGER = Logger.getLogger(Calculator.class);
+
+    public int add(int firstNumber, int secondNumber) {
+        LOGGER.info("Adding two numbers " + firstNumber + " and " + secondNumber);
+        return firstNumber + secondNumber;
+    }
+
+    public int addMultipleNumbers(int[] numbers) {
+        int total = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            total += numbers[i];
+        }
+        return total;
+    }
 }
